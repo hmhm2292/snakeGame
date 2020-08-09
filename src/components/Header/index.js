30205435a28565cb3ca1fdc30c0023c934bc5b7d
hmhm2292 <hyungmok.kim92@gmail.com>
2020-08-09 16:29:55 +0900

    ScoreModal Added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 const Header = ({score}) => {
   return (
     <View style={styles.header}>
       <Text style={styles.score}>SCORE: {score}</Text>
+      <TouchableOpacity>
+        <Text style={styles.score}>Score Board</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -14,10 +17,13 @@ export default Header;
 const styles = StyleSheet.create({
   header: {
     borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
   },
   score: {
-    marginVertical: 10,
-    marginHorizontal: 20,
     color: 'teal',
     fontWeight: 'bold',
     fontSize: 20,
