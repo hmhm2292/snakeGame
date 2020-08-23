0a40252137bc9ca42227107de8f6a713104161f9
hmhm2292 <hyungmok.kim92@gmail.com>
2020-08-23 22:33:09 +0900

    Added Scoreboard modal

--- src/redux/sagas/scoreBoard.js
+++ src/redux/sagas/scoreBoard.js
@@ -1,14 +1,37 @@
 import {takeLatest, put, call} from 'redux-saga/effects';
-import {FETCH_SCORE_BOARD, updateScoreBoard} from '../reducers/scoreBoard';
+import {
+  FETCH_SCORE_BOARD,
+  updateScoreBoard,
+  POST_NEW_SCORE,
+} from '../reducers/scoreBoard';
 import {setAppState} from '../reducers/appState';
-import {scoreBoardResponse} from '../api/scoreBoardApi';
+import {fetchScore, uploadScore} from '../api/scoreBoardApi';
+import reactotron from 'reactotron-react-native';
 
 export function* fetchScoreBoard() {
-  yield put(setAppState('START_FETCH_SCORE_BOARD'));
-  const scoreBoardData = yield call(scoreBoardResponse);
+  reactotron.log('fetch');
+  yield put(setAppState('START_FETCH_SCORE_BOARD', null, true));
+  const scoreBoardData = yield call(fetchScore);
   yield put(updateScoreBoard(scoreBoardData));
+  yield put(setAppState('SUCCESS_FETCH_SCORE_BOARD', null, false));
 }
 
-const scoreBoardSagas = [takeLatest(FETCH_SCORE_BOARD, fetchScoreBoard)];
+export function* postNewScore(action) {
+  reactotron.log(action);
+  yield put(setAppState('START_UPDATE_SCORE_BOARD'));
+  const response = yield call(uploadScore, action);
+
+  reactotron.log({response});
+  if (response.status === 200) {
+    yield put(setAppState('SUCCESS_UPDATE_SCORE_BOARD'));
+  } else {
+    yield put(setAppState('FAILED_UPDATE_SCORE_BOARD'));
+  }
+}
+
+const scoreBoardSagas = [
+  takeLatest(FETCH_SCORE_BOARD, fetchScoreBoard),
+  takeLatest(POST_NEW_SCORE, postNewScore),
+];
 
 export default scoreBoardSagas;
